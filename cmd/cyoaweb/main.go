07ec226f58c2b7e35abb6d155a90e8d3967f3687
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2025-06-05 23:48:21 -0700

    Parsed the gopher.json file to a Story struct
    Signed-off-by: Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
+
+	"learningGo/Gophercises/cyoa"
 )
 
 // go build builds a binary and name it with the name of the directory
+// Write a working solution
+// Refactor (create functions, new structs to improve readability)
 func main() {
-	fmt.Println("hi")
+	filePath := "/Users/ronald/Documents/Online-Courses/LearningGo/Gophercises/cyoa/gopher.json"
+	fileName := flag.String("file", filePath, "the JSON file with CYOA story")
+	flag.Parse()
+	fmt.Printf("Using the story in %s. \n", *fileName)
+	file, err := os.Open(*fileName)
+	if err != nil {
+		log.Fatal("Error opening file: ", err)
+	}
+
+	story, err := cyoa.JsonStory(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", story)
 }
--- story.go
+++ story.go
@@ -2,6 +2,8 @@ package cyoa
 
 import (
 	"encoding/json"
+	"io"
+	"log"
 )
 
 // Store the story in a map
@@ -16,4 +18,17 @@ type Chapter struct {
 type Option struct {
 	Text    string
 	Chapter string
+}
+
+// Decode the file and return a Story object
+func JsonStory(r io.Reader) (Story, error) {
+	var story Story
+	decoder := json.NewDecoder(r)
+
+	if err := decoder.Decode(&story); err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return story, nil
+
 }
