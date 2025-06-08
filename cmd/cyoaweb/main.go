12f1c2d7f8e452b869f96e0c76b1d898f43af91e
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2025-06-07 18:07:41 -0700

    Parsing the path and serving it to an HTTP request
    Signed-off-by: Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"learningGo/Gophercises/cyoa"
@@ -13,6 +14,7 @@ import (
 // Write a working solution
 // Refactor (create functions, new structs to improve readability)
 func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
 	filePath := "/Users/ronald/Documents/Online-Courses/LearningGo/Gophercises/cyoa/gopher.json"
 	fileName := flag.String("file", filePath, "the JSON file with CYOA story")
 	flag.Parse()
@@ -27,5 +29,9 @@ func main() {
 		log.Fatal(err)
 	}
 
+	handler := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port: %d \n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), handler))
+	fmt.Printf("done serving the request")
 	fmt.Printf("%+v\n", story)
 }
--- story.go
+++ story.go
@@ -2,22 +2,26 @@ package cyoa
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
+	"net/http"
+	"strings"
+	"text/template"
 )
 
 // Store the story in a map
 type Story map[string]Chapter
 
 type Chapter struct {
-	Title     string   `json:"title"`
-	Paragraps []string `json:"story"`
-	Options   []Option `json:"options"`
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
 }
 
 type Option struct {
-	Text    string
-	Chapter string
+	Text string `json:"text"`
+	Chapter  string `json:"arc"`
 }
 
 // Decode the file and return a Story object
@@ -26,9 +30,80 @@ func JsonStory(r io.Reader) (Story, error) {
 	decoder := json.NewDecoder(r)
 
 	if err := decoder.Decode(&story); err != nil {
-		log.Fatal(err)
 		return nil, err
 	}
 	return story, nil
+
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTemplate))
+}
+
+// easier to package it inside our source code
+var defaultHandlerTemplate = `
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Choose Your Own adventure.</title>
+	</head>
+	<body>
+		<h1>{{.Title}}</h1>
+		<p>{{range .Paragraphs}}</p>
+			<p>{{.}}</p>
+		{{end}}
+
+		<ul>
+			{{range .Options}}
+				<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+			{{end}}
+		</ul>
+		
+	</body>
+	</html>
+`
+
+type handler struct {
+	s Story
+}
+
+// It is better to return an interface rather than returning a handler type
+// because when we generate the godoc -http:8080, it makes it easier to see what
+// methods are under the interface.
+// If we were to return the handler type, the methods under the interface
+// won't get exported.
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+// ServehTTP can be called by a handler object
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Serving HTTP request...")
+	path := strings.TrimSpace(r.URL.Path)
+
+	// edge case: no path at all or root path
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+	// "/intro" => "intro"
+	path = path[1:]
+	fmt.Printf("--- the path is: %s \n", path)
+	if chapter, ok := h.s[path]; ok {
+		err := tpl.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			// Don't expose development errors to the end users because it could contain something
+			// we don't want them to see like our Database password.
+			http.Error(w, "Something went wrong..", http.StatusInternalServerError)
+		}
+		// Found the chapter
+		fmt.Println("found the chapter")
+		fmt.Printf("the chapter is: %s", chapter)
+		return
+	}
+
+	http.Error(w, "Chapter not found.", http.StatusNotFound)
 
 }
